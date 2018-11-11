55ed5dbd8aac83eedee6f097a93aa170deb288ea
Uma Sankar <34178439+umasankar1999@users.noreply.github.com>
2018-11-11 23:56:15 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("main").style.marginRight = "350px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.1)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
